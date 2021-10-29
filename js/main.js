3ae7dced0df0fb8121dae5bbba7669a360e42ebb
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-10-29 18:03:42 -0300

    fix: cep mask

--- js/main.js
+++ js/main.js
@@ -1,22 +1,61 @@
 const el = document.getElementById("box");
-const regexCep = /\d{3}\d{3}\d{2}/;
+
+let locale = document.getElementById("locale");
+let cases = document.getElementById("confirmedCases");
+let suspects = document.getElementById("suspectedCases");
+let deaths = document.getElementById("totalDeaths");
+let invalidCep = document.getElementById("invalidCep");
+
+const invalidCepMessage = "CEP inválido, revise os números digitados.";
+
+const regexCep = /(^[0-9]{5})-?([0-9]{3}$)/;
+
+let zipCode = "";
+el.addEventListener("keyup", ({ key }) => {
+  zipCode = el.value;
+  if (zipCode)
+    if (zipCode.length === 6) {
+      el.value = `${zipCode.substr(0, 5)}-${zipCode.substr(5, 9)}`;
+      console.log(zipCode);
+    }
+  if (key === "Enter") {
+    toggleShow();
+  }
+});
+
+// el.addEventListener("onkeyup", mask.init(this, mask.cep));
+// const mask = {
+//   init: function (object, mask) {
+//     let obj = object;
+//     let mask = mask;
+//     setTimeout("mask.execMask", 1);
+//   },
+
+//   execMask: function () {
+//     obj.value = mask(obj.value);
+//   },
+
+//   cep: function (cep) {
+//     cep = cep.replace(/\D/g, "");
+//     cep = cep.replace(/^(\d{2})(\d)/, "$1.$2");
+//     cep = cep.replace(/\.(\d{3})(\d)/, ".$1-$2");
+//     return cep;
+//   },
+// };
+
+el.add;
 
 function toggleShow() {
-  var invalidCep = document.getElementById("invalidCep");
   invalidCep.innerHTML = "";
 
   function validateCep() {
     if (!regexCep.test(el.value.toString())) {
-      invalidCep.classList.add("invalidCepMessage");
-      invalidCep.innerHTML = "CEP inválido, revise os números digitados.";
-      console.log("teste cep invaldio");
-      tableValues.innerHTML = "";
-      console.log(!regexCep.test(el.value.toString()));
+      addInvalidCepAttributes();
     } else {
       invalidCep.classList.remove("invalidCepMessage");
       invalidCep.innerHTML = "";
+
       getContent();
-      console.log("teste valdio");
     }
   }
 
@@ -24,50 +63,41 @@ function toggleShow() {
 
   async function getContent() {
     try {
-      var cepId = el.value.toString();
-      const responseCep = await fetch(`http://localhost:3333/ceps/${cepId}`);
-      console.log("responseCep", responseCep);
-      console.log(cepId);
-      const cepInfo = await responseCep.json();
+      let cepId = el.value.toString();
 
-      console.log("cepInfo", cepInfo);
+      const responseCep = await fetch(`http://localhost:3333/ceps/${cepId}`);
+      const cepInfo = await responseCep.json();
 
       const responseCovid = await fetch(
         `https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${cepInfo.uf}`
       );
-      console.log(responseCovid);
       const covidInfo = await responseCovid.json();
 
-      if (cepInfo != undefined && covidInfo != undefined) {
-        showData(cepInfo, covidInfo);
-      } else {
-        const invalidCep = document.getElementById("invalidCep");
-        invalidCep.classList.toggle("redBorder");
-      }
+      showData(cepInfo, covidInfo);
     } catch (error) {
-      console.log(error);
+      console.log("Ocorreu um erro ;(", error);
     }
   }
 
   function showData(cepInfo, covidInfo) {
-    let localeFound = "";
-    localeFound += `${cepInfo.localidade}`;
+    if (cepInfo.localidade === undefined) {
+      addInvalidCepAttributes();
+    } else {
+      locale.innerHTML = `${cepInfo.localidade}`;
+      cases.innerHTML = `${covidInfo.cases}`;
+      suspects.innerHTML = `${covidInfo.suspects}`;
+      deaths.innerHTML = `${covidInfo.deaths}`;
+    }
+  }
 
-    document.getElementById("locale").innerHTML = localeFound;
+  function addInvalidCepAttributes() {
+    invalidCep.classList.add("invalidCepMessage");
+    invalidCep.innerHTML = invalidCepMessage;
 
-    let confirmedCases = "";
-    confirmedCases += `${covidInfo.cases}`;
-
-    document.getElementById("confirmedCases").innerHTML = confirmedCases;
-
-    let suspectedCases = "";
-    suspectedCases += `${covidInfo.suspects}`;
-
-    document.getElementById("suspectedCases").innerHTML = suspectedCases;
-
-    let totalDeaths = "";
-    totalDeaths += `${covidInfo.deaths}`;
-
-    document.getElementById("totalDeaths").innerHTML = totalDeaths;
+    locale.innerHTML =
+      cases.innerHTML =
+      suspects.innerHTML =
+      deaths.innerHTML =
+        "";
   }
 }
